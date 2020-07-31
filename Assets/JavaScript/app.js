fc5f22d7c8d6ad429c73c9fbe52e7aa6d2ebd06a
jcostopulos <costopulos@gmail.com>
2020-07-30 18:09:29 -0700

    Added two more buttons and linked them to sound files

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -1,7 +1,21 @@
 
 
-var x = document.getElementById("myAudio");
+var lied = document.getElementById("lied");
+var ask = document.getElementById("ask");
+var bastard = document.getElementById("bastard")
 
-function playAudio() {
-    x.play();
+
+
+
+
+function playLied() {
+    lied.play();
+}
+
+function playAsk() {
+    ask.play();
+}
+
+function playBastard() {
+    bastard.play();
 }
