108e72c5867d324c0e83e960b469c625848c4e8f
jcostopulos <costopulos@gmail.com>
2020-07-31 06:59:29 -0700

    added and linked stylesheet

--- Assets/JavaScript/app.js
+++ Assets/JavaScript/app.js
@@ -1,21 +1,20 @@
 
-
+// vars store audio file from buttons audio methods.
 var lied = document.getElementById("lied");
 var ask = document.getElementById("ask");
 var bastard = document.getElementById("bastard")
 
-
-
-
-
+//function per button to play individual sound files
+//------------------
 function playLied() {
     lied.play();
 }
-
+//------------------
 function playAsk() {
     ask.play();
 }
-
+//------------------
 function playBastard() {
     bastard.play();
 }
+//------------------
